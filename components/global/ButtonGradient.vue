f3e4466aa4ad936815566784c6ef3794467f7ebc
Oliver Gosling <oli@big.fan>
2022-01-21 14:44:36 +0000

    add buttons as global components to be used in markdown files

---
+++ components/global/ButtonGradient.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="href.startsWith('http')">
+    <a
+      :href="href"
+      target="_blank"
+      class="gradient-button py-3.5 px-8 text-navy rounded-full inline-block text-center !no-underline"
+      ><slot></slot
+    ></a>
+  </div>
+  <NuxtLink
+    class="gradient-button py-3.5 px-8 text-navy rounded-full inline-block text-center !no-underline"
+    v-else
+    :to="href"
+    target="_self"
+    :class="buttonClass"
+    ><slot></slot
+  ></NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.gradient-button {
+  background-image: linear-gradient(
+    90deg,
+    rgba(253, 169, 255, 1) 0%,
+    rgba(201, 184, 255, 1) 14%,
+    rgba(77, 255, 229, 1) 50%
+  );
+  background-size: 200% 50%;
+  transition: background-position 2s;
+}
+
+.gradient-button:hover {
+  animation-name: gradientWave;
+  background-position: 100% 50%;
+}
+</style>
---
+++ components/global/ButtonLink.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="link.startsWith('http')">
+    <a
+      :href="link"
+      :target="target"
+      class="border text-xl px-11 py-3 rounded-full inline-block !no-underline border-midGrey button"
+      ><slot></slot
+    ></a>
+  </div>
+  <NuxtLink
+    v-else
+    :to="link"
+    :target="target"
+    class="border text-xl px-11 py-3 rounded-full inline-block !no-underline border-midGrey button"
+    ><slot></slot
+  ></NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      default: 'light',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+    link: {
+      type: String,
+      default: '',
+    },
+    target: {
+      type: String,
+      default: '_self',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.button {
+  border: solid 1px transparent;
+  background-image: linear-gradient(#d8d8d8, #d8d8d8),
+    linear-gradient(#d8d8d8, #d8d8d8);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+  box-shadow: 2px 1000px 1px #fff inset;
+  transition: background-image 3s ease-in-out;
+  @apply text-navy;
+  @apply text-xl;
+  @apply py-3;
+  @apply px-11;
+  @apply rounded-full;
+}
+
+.button:hover {
+  background-image: linear-gradient(#16183c, #16183c),
+    linear-gradient(101deg, #fda9ff, #4dffe5);
+}
+</style>
