9d4bef612404fe20f99466003ff7499651c3904b
Oliver Gosling <oli@big.fan>
2022-02-01 14:28:14 +0000

    fix light mode icon

--- components/UI/LightMode.vue
+++ components/UI/LightMode.vue
@@ -4,10 +4,13 @@
       class="w-[53px] h-[53px] flex md:justify-end items-center"
       @click="toggleLightMode"
     >
-      <div v-if="$colorMode.value === 'light'">
-        <SVGLightModeButton class="w-[25px] h-[25px]" />
+      <div class="w-full h-full flex justify-center items-center">
+        <SVGLightModeButton
+          v-if="$colorMode.value === 'light'"
+          class="w-[25px] h-[25px]"
+        />
+        <SVGDarkModeButton v-else class="w-[20px] h-[20px]" />
       </div>
-      <div v-else><SVGDarkModeButton class="w-[20px] h-[20px]" /></div>
     </button>
   </div>
 </template>
