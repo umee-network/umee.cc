fdf6dfe6202b67b545c9b434a1e108da95423eac
Rob Sherwani <sherwanirobert@gmail.com>
2022-05-21 19:36:52 -0400

    update gtm tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -161,8 +161,31 @@ export default {
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
     '@nuxtjs/markdownit',
+    '@nuxtjs/gtm',
   ],
+  gtm: {
+    enabled: true /* see below */,
+    debug: false,
 
+    id: 'GTM-N3TTX43',
+    layer: 'dataLayer',
+    variables: {},
+
+    pageTracking: false,
+    pageViewEventName: 'nuxtRoute',
+
+    autoInit: true,
+    respectDoNotTrack: true,
+
+    scriptId: 'gtm-script',
+    scriptDefer: false,
+    scriptURL: 'https://www.googletagmanager.com/gtm.js',
+    crossOrigin: false,
+
+    noscript: true,
+    noscriptId: 'gtm-noscript',
+    noscriptURL: 'https://www.googletagmanager.com/ns.html',
+  },
   colorMode: {
     classSuffix: '',
   },
