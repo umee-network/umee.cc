bdc7e841194cb545d9e86cd29f283d3e9078eda1
Oliver Gosling <oli@big.fan>
2022-06-28 16:13:58 +0100

    upgraded nav wip

---
+++ components/Navigation/Build.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mx-auto pb-16">
+    <div class="grid grid-cols-2 gap-x-12">
+      <div>
+        <div
+          class="text-xs uppercase tracking-widest border-b border-darkGreyOnNavy dark:text-navy mb-5 pb-1"
+        >
+          Build
+        </div>
+        <ul class="text-xl">
+          <li><a href="#" class="inline-block py-1.5">About Umee</a></li>
+          <li>
+            <a href="#" class="inline-block py-1.5">Blockchain Basics</a>
+          </li>
+          <li><a href="#" class="inline-block py-1.5">Blog</a></li>
+        </ul>
+      </div>
+      <div>
+        <div
+          class="text-xs uppercase tracking-widest border-b border-darkGreyOnNavy mb-5 pb-1"
+        >
+          Guides
+        </div>
+        <div class="grid grid-cols-2">
+          <ul class="text-xl">
+            <li>
+              <a href="#" class="inline-block py-1.5">Creating a Wallet</a>
+            </li>
+            <li>
+              <a href="#" class="inline-block py-1.5">Funding a Wallet</a>
+            </li>
+          </ul>
+          <ul class="text-xl">
+            <li>
+              <a href="#" class="inline-block py-1.5">Staking UMEE</a>
+            </li>
+            <li>
+              <a href="#" class="inline-block py-1.5">Lending & Borrowing</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
