9ba79ce9de6e8a4e714564d098d7127e40029f9d
Ricardo Silva <ricardo.jps@outlook.com>
2023-08-04 11:38:49 +0100

    fix: removed some warnings

--- pages/ecosystem.vue
+++ pages/ecosystem.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="container pb-12 md:pb-20 lg:pb-28">
-
-    <div class="grid grid-cols-1 lg:columns-2 lg:flex justify-center align-middle font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple to-aqua mb-4">
-      <div class="flex justify-center" >
-        <h1 class="leading-none font-serif lg:text-[400px] md:text-[375px] text-[200px]">14</h1>
+    <div
+      class="grid grid-cols-1 lg:columns-2 lg:flex justify-center align-middle font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple to-aqua mb-4"
+    >
+      <div class="flex justify-center">
+        <h1
+          class="leading-none font-serif lg:text-[400px] md:text-[375px] text-[200px]"
+        >
+          14
+        </h1>
       </div>
 
-      <div class="flex flex-col lg:justify-center lg:items-stretch items-center width=100%">
-        <h1 class="uppercase lg:tracking-[1rem] font-serif leading-[0.7] lg:text-[190px] md:text-[140px] text-[80px] lg:pb-0 pb-2">Apps and</h1>
-        <h1 class="uppercase tracking-[-.65rem] font-serif leading-[0.75] lg:text-[250px] md:text-[200px] text-[112px] lg:pb-0 pb-2" >Services</h1>
+      <div
+        class="flex flex-col lg:justify-center lg:items-stretch items-center width=100%"
+      >
+        <h1
+          class="uppercase lg:tracking-[1rem] font-serif leading-[0.7] lg:text-[190px] md:text-[140px] text-[80px] lg:pb-0 pb-2"
+        >
+          Apps and
+        </h1>
+        <h1
+          class="uppercase tracking-[-.65rem] font-serif leading-[0.75] lg:text-[250px] md:text-[200px] text-[112px] lg:pb-0 pb-2"
+        >
+          Services
+        </h1>
       </div>
     </div>
 
@@ -21,22 +36,32 @@
       Join the Umee ecosystem today
     </h1>
     <div class="grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-8">
-      <div v-for="app in apps" :key="app.name" class="col-span-1 flex md:justify-start justify-center md:gap-0 gap-8">
+      <div
+        v-for="app in apps"
+        :key="app.name"
+        class="col-span-1 flex md:justify-start justify-center md:gap-0 gap-8"
+      >
         <a :href="app.web" target="_blank"
-          ><img :src="app.img" class="w-24 h-24 rounded-lg mr-4"
+          ><img alt="" :src="app.img" class="w-24 h-24 rounded-lg mr-4"
         /></a>
         <div>
           <h3 class="font-xl font-bold">{{ app.name }}</h3>
           <p class="font-md">{{ app.description }}</p>
           <div class="flex mt-4">
             <a class="mr-2" :href="app.web" target="_blank">
-              <img src="~/assets/images/ecosystem-icons/docs.png" />
+              <img alt="docs" src="~/assets/images/ecosystem-icons/docs.png" />
             </a>
             <a class="mr-2" :href="app.github" target="_blank">
-              <img src="~/assets/images/ecosystem-icons/github.png" />
+              <img
+                alt="github"
+                src="~/assets/images/ecosystem-icons/github.png"
+              />
             </a>
             <a :href="app.twitter" target="_blank">
-              <img src="~/assets/images/ecosystem-icons/twitter.png" />
+              <img
+                alt="twitter"
+                src="~/assets/images/ecosystem-icons/twitter.png"
+              />
             </a>
           </div>
         </div>
@@ -48,8 +73,7 @@
 <script>
 export default {
   async asyncData({ $content }) {
-    const ecosystemContent = await $content('pages/ecosystem').fetch()
-    return ecosystemContent
+    return await $content('pages/ecosystem').fetch()
   },
   head() {
     const title = 'Umee: Ecosystem'
